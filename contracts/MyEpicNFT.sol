49ea5fed7792b05e13887fe9ea5f5c64b677ad1e
Lee <ljl1997@gmail.com>
2021-11-28 01:50:21 +0800

    Removed some comments

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -13,7 +13,6 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  // We split the SVG at the part where it asks for the background color.
   string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
   string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
@@ -21,10 +20,8 @@ contract MyEpicNFT is ERC721URIStorage {
   string[] secondWords = ["APPLE", "BALANCE", "CORN", "DOGE", "ETF", "FISH"];
   string[] thirdWords = ["HELP", "ASSIST", "STAND", "IGNORE", "PASSIVE", "ACTIVE"];
 
-  // Get fancy with it! Declare a bunch of colors.
   string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
 
-  // MAGICAL EVENTS.
   event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   constructor() ERC721 ("SquareNFT", "SQUARE") {
@@ -70,17 +67,14 @@ contract MyEpicNFT is ERC721URIStorage {
     string memory third = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-    // Add the random color in.
     string memory randomColor = pickRandomColor(newItemId);
     string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
-    // Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
         bytes(
             string(
                 abi.encodePacked(
                     '{"name": "',
-                    // We set the title of our NFT as the generated word.
                     combinedWord,
                     '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
                     // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
