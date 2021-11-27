578a631214d159740edddc0ff17ab83bb0379f2c
Lee <ljl1997@gmail.com>
2021-11-27 14:55:10 +0800

    Updated Contract with random background NFT minting

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -13,11 +13,16 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  // We split the SVG at the part where it asks for the background color.
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   string[] firstWords = ["ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"];
   string[] secondWords = ["APPLE", "BALANCE", "CORN", "DOGE", "ETF", "FISH"];
   string[] thirdWords = ["HELP", "ASSIST", "STAND", "IGNORE", "PASSIVE", "ACTIVE"];
+
+  // Get fancy with it! Declare a bunch of colors.
+  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
 
   // MAGICAL EVENTS.
   event NewEpicNFTMinted(address sender, uint256 tokenId);
@@ -43,6 +48,13 @@ contract MyEpicNFT is ERC721URIStorage {
     rand = rand % thirdWords.length;
     return thirdWords[rand];
   }
+  
+  // Same old stuff, pick a random color.
+  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
 
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
@@ -56,7 +68,9 @@ contract MyEpicNFT is ERC721URIStorage {
     string memory third = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-    string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+    // Add the random color in.
+    string memory randomColor = pickRandomColor(newItemId);
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
     // Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
@@ -86,13 +100,10 @@ contract MyEpicNFT is ERC721URIStorage {
 
     _safeMint(msg.sender, newItemId);
     
-    // Update your URI!!!
     _setTokenURI(newItemId, finalTokenUri);
   
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
-
-    // EMIT MAGICAL EVENTS.
     emit NewEpicNFTMinted(msg.sender, newItemId);
   }
 }
